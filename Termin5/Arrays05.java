0c6bf2ac367d60b9d6750465531b0e1e74010660
Alexander <33837034+mittey68@users.noreply.github.com>
2024-02-28 16:13:33 +0100

    create arrays5

---
+++ Termin5/Arrays05.java
@@ -0,0 +1,35 @@
+package Termin5;
+
+
+/**
+ * Beschreiben Sie hier die Klasse Arrays05.
+ * 
+ * @author (Ihr Name) 
+ * @version (eine Versionsnummer oder ein Datum)
+ */
+public class Arrays05
+{
+    // Instanzvariablen - ersetzen Sie das folgende Beispiel mit Ihren Variablen
+    private int x;
+
+    /**
+     * Konstruktor für Objekte der Klasse Arrays05
+     */
+    public Arrays05()
+    {
+        // Instanzvariable initialisieren
+        x = 0;
+    }
+
+    /**
+     * Ein Beispiel einer Methode - ersetzen Sie diesen Kommentar mit Ihrem eigenen
+     * 
+     * @param  y    ein Beispielparameter für eine Methode
+     * @return        die Summe aus x und y
+     */
+    public int beispielMethode(int y)
+    {
+        // tragen Sie hier den Code ein
+        return x + y;
+    }
+}
