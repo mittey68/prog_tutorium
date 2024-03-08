11ef4c12fce3dc12d445e90ba3eee82d12ecdaee
Alexander <33837034+mittey68@users.noreply.github.com>
2024-03-08 22:03:34 +0100

    Update ExamTask02.java

--- Termin6/Musterklausur/Aufgabe2/ExamTask02.java
+++ Termin6/Musterklausur/Aufgabe2/ExamTask02.java
@@ -11,7 +11,8 @@ public class ExamTask02 { // 0,5
         lectures.add(new Lecture("Mathe", 5)); // 0,5
         lectures.add(new Lecture("Programmierung", 10)); // 0,5
 
-        // Vorher: Class class1 = new Class("WWIBE122", CourseOfStudies.WI, lectures); // 0,5
+        // Vorher: 
+        // Class class1 = new Class("WWIBE122", CourseOfStudies.WI, lectures); // 0,5
         Class class1 = new Class("WWIBE122", CourseOfStudies.WI); // 0,5
         class1.addStudent(new Student("8271625", "Hans Maier")); // 0,5
         class1.addStudent(new Student("9102934", "Peter Müller")); // 0,5
