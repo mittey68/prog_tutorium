1cf9451b97611c17ed0d740e3c88b3046f8b04c1
Alexander <33837034+mittey68@users.noreply.github.com>
2024-03-15 10:04:48 +0100

    Parking garage implemeted

---
+++ Termin7/Tiefgarage/Car.java
@@ -0,0 +1,8 @@
+package Termin7.Tiefgarage;
+
+public class Car extends Vehicle{
+    // Konstruktor
+    public Car(String make, String model, double lengthInM, double heigtInM) {
+        super(make, model, lengthInM, heigtInM);
+    }
+}
